208781e68f2df199c20c7634139828deebb4d5fc
Polestar <132473028+P0LESTAR@users.noreply.github.com>
2026-05-10 03:27:48 +0900

    gitignore 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   darkMode: "class",
@@ -66,5 +68,5 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [forms],
 };
